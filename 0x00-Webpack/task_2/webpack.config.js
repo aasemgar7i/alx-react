ca94a3c768a130b14ff916bd5ecefaf4970dbb93
aasemgar7i <aasemgar7i@gmail.com>
2024-07-23 17:20:08 +0300

    up35

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/dashboard_main.js',
-  output: {
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+    output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
