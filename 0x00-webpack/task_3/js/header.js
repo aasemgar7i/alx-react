832de1dcbde8b719d2c9f11442fde615a9ee2fc9
aasemgar7i <aasemgar7i@gmail.com>
2024-07-23 14:15:51 +0300

    webpack100

---
+++ 0x00-webpack/task_3/js/header.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import '../css/header.css';
+
+console.log('Init header');
+
+$('body').prepend('<div id="logo"></div>');
+$('body').prepend('<h1>Holberton Dashboard</h1>');
