846dec483272663c1143690f6d32b269feefa25a
aasemgar7i <aasemgar7i@gmail.com>
2024-07-23 17:23:27 +0300

    bak

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
-    output: {
+  entry: './js/dashboard_main.js',
+  output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  mode: 'production',
   module: {
     rules: [
       {
